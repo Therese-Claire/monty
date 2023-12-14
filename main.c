3ebda980b410cbeed96f051396423221187c4051
Therese-Claire <awonguagabi@gmail.com>
2023-12-14 14:00:18 -0800

    function update

--- main.c
+++ main.c
@@ -1,19 +1,22 @@
 #include "monty.h"
+
 bus_t bus = {NULL, NULL, NULL, 0};
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
-int main(int argc, char *argv[])
+ * main - monty program entry point
+ *
+ * @argc: number of arguments passed to the monty program
+ * @argv: pointer to the arguments
+ *
+ * Return: 0 (SUCCESS)
+ */
+int main(int argc, char **argv)
 {
-	char *content;
-	FILE *file;
+	FILE *file = NULL;
+	char *line = NULL;
+	stack_t *top = NULL;
+	ssize_t lines_read = 1;
 	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
+	unsigned int line_number = 0;
 
 	if (argc != 2)
 	{
@@ -21,25 +24,25 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
-	bus.file = file;
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (read_line > 0)
+	bus.file = file;
+	while (lines_read > 0)
 	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
+		line = NULL;
+		lines_read = getline(&line, &size, file);
+		bus.line = line;
+		line_number++;
+		if (lines_read > 0)
 		{
-			execute(content, &stack, counter, file);
+			exec(&top, line_number);
 		}
-		free(content);
+		free(line);
 	}
-	free_stack(stack);
+	free_stack(top);
 	fclose(file);
-return (0);
+	return (0);
 }

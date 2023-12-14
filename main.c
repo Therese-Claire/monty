153b7f231cdd999bcd44ade302720c9082f9c5b2
Therese-Claire <awonguagabi@gmail.com>
2023-12-14 13:15:40 -0800

    added global structure

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
 * main - monty code interpreter
 * @argc: number of arguments
@@ -20,6 +21,7 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
+	bus.file = file;
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
@@ -29,6 +31,7 @@ int main(int argc, char *argv[])
 	{
 		content = NULL;
 		read_line = getline(&content, &size, file);
+		bus.content = content;
 		counter++;
 		if (read_line > 0)
 		{
